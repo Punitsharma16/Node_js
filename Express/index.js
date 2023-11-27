978881d4f79efdd21dc3ee1f5901ef17d10ef877
punit sharma <punitsharma9034908543@gmail.com>
2023-11-27 17:09:51 +0530

    starting node

---
+++ Express/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4500;
+app.listen(PORT,()=>{
+    console.log('App is running');
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello Everyone');
+});
+
+app.get('/profile',(req,res)=>{
+    res.send('<h2>Punit sharma</h2>');
+})
